557a928b65007b7a86148c4c2a90fa043382e7cd
duzhuoyan <duzhuoyan1994@163.com>
2024-04-03 22:47:35 +0800

    HelloWorld

---
+++ flink/src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * @author ：duzhuoyan
+ * @date ：Created in 2024/4/3 22:41
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+    }
+
+}
---
+++ spark/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @author ：duzhuoyan
+ * @date ：Created in 2024/4/3 22:45
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+
+    }
+
+}
